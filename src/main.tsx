6f30f01de7b42b3f84841fad16a7221415ba6d13
Aayush Shrestha <aayushshres20@gmail.com>
2024-05-15 09:46:25 +0530

    added starfield

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import "./style.css";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"; //24:50
+import getStarfield from "./components/starfield";
 import gsap from "gsap";
 
 // create scene
@@ -9,11 +10,15 @@ const scene = new THREE.Scene();
 // create sphere
 const geometry = new THREE.SphereGeometry(4, 64, 64);
 const material = new THREE.MeshStandardMaterial({
-  color: "#00ff83",
-  roughness: 0.4,
+  color: "#00cafc",
+  roughness: 0.5,
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+// create starfield
+const stars = getStarfield();
+scene.add(stars);
 
 // size
 const sizes = {
@@ -31,9 +36,10 @@ const camera = new THREE.PerspectiveCamera(
   45,
   sizes.width / sizes.height,
   0.1,
-  100
+  1000
 );
-camera.position.z = 20;
+camera.position.z = 200;
+camera.updateProjectionMatrix();
 scene.add(camera);
 
 // renderer
@@ -45,8 +51,6 @@ renderer.setPixelRatio(2);
 // controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-controls.enablePan = false;
-controls.enableZoom = false;
 controls.autoRotate = true;
 
 // resize
@@ -73,6 +77,7 @@ const tl = gsap.timeline({
   defaults: { duration: 1 },
 });
 tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+tl.fromTo(stars.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
 tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
 
 let mouseDown = false;
