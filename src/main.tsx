91f069ba0e04e06eb87ab7ce78c5489bb2f429d9
Aayush Shrestha <aayushshres20@gmail.com>
2024-05-15 09:09:06 +0530

    added color animation

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(4, 64, 64);
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
+  roughness: 0.4,
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
@@ -21,7 +22,7 @@ const sizes = {
 };
 
 // add light
-const light = new THREE.PointLight(0xffffff, 50, 20);
+const light = new THREE.PointLight(0xffffff, 100, 20);
 light.position.set(0, 10, 10);
 scene.add(light);
 
@@ -73,3 +74,25 @@ const tl = gsap.timeline({
 });
 tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
 tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
+
+let mouseDown = false;
+let rgb = [];
+
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ];
+    const newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
